ea30e4653eceac9b51884fa5d4171c2a7b8c2d08
Maya Treacy <mayatreacy@gmail.com>
2020-06-04 12:29:49 +1000

    put back MainNavPublic in routes js

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import MainNavPublic from "./js/components/MainNavPublic";
+// import MainNavPublic from "./js/components/MainNavPublic";
 import Home from "./js/Home";
+import { Navbar, Nav } from "react-bootstrap";
+
 
 export default (
   <BrowserRouter>
     <div>
-      <MainNavPublic />
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">
+            <img
+              className="d-inline-block align-top"
+              src="public/assets/images/AnitaBLogo.png"
+              alt="Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/programs">Programs</Nav.Link>
+              <Nav.Link href="/organizations">Organizations</Nav.Link>
+              <Nav.Link href="/members">Members</Nav.Link>
+              <Nav.Link href="/register">Register</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
