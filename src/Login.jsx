7426086c2d72a2de0b1a236fdcf94fafeeeda46f
Maya Treacy <mayatreacy@gmail.com>
2020-06-20 17:27:15 +1000

    Feat: Allow user to Login issue#14

--- src/Login.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Login() {
-    return (
-      <div>This is the Login page</div>
-   );
-}  
---
+++ src/tests/Login.test.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, fireEvent, screen, waitForElement } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Login from "../login/Login";
+import { act } from 'react-dom/test-utils';
+
+
+const server = setupServer(
+    rest.post('http://127.0.0.1:5000/login', (req, res, ctx) => {
+        const payload = {
+            username: "MyUsername",
+            password: "12345678",
+        };
+        expect(req.body).toEqual(payload)
+        return res(ctx.json({ 
+            access_token: "fake_access_token",
+            access_expiry: 1594771200
+        }))
+    })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+
+
+it('allows the user to login successfully', async () => {
+    render(<Login />)
+
+    fireEvent.change(screen.getByPlaceholderText('Username or Email'), {
+        target: { value: 'MyUsername' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Password'), {
+        target: { value: '12345678' },
+    })
+    expect(screen.queryByLabelText("errorMessage")).not.toBeInTheDocument();
+    
+    act(() => {
+        fireEvent.click(screen.getByRole('button', { name: "Login" }), {
+            target: { value: 'true' },
+        })
+    });
+    expect(screen.queryByLabelText("errorMessage")).not.toBeInTheDocument();
+})
+
+it('handles wrong credentials', async () => {
+    server.use(
+      rest.post('http://127.0.0.1:5000/login', (req, res, ctx) => {
+        const wrongPayload = {
+            username: "MyUsername",
+            password: "87654321",
+        };
+        expect(req.body).toEqual(wrongPayload)
+        return res(ctx.status(401), ctx.json({message: 'Username or password is wrong.'}))
+      }),
+    )
+   
+    render(<Login />)
+   
+    fireEvent.change(screen.getByPlaceholderText('Username or Email'), {
+        target: { value: 'MyUsername' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Password'), {
+        target: { value: '87654321' },
+    })
+    expect(screen.queryByLabelText("errorMessage")).not.toBeInTheDocument();
+    
+    fireEvent.click(screen.getByRole('button', { name: "Login" }), {
+        target: { value: 'true' },
+    })
+
+    await waitForElement(() => screen.getByLabelText('errorMessage'))
+    
+    expect(screen.getByLabelText('errorMessage')).toHaveTextContent("Username or password is wrong.")
+  })
