1e024238a6f6c8a1a9fec885567c253c03226dad
Tapajyoti Bose <44058757+ruppysuppy@users.noreply.github.com>
2020-09-06 13:25:57 +0530

    feat: added password confirmation field on Registration page(#89)
    * feat(register): added password confirmation field
    * fix(register): fixed tests failing + adding signup blocking (password mismatch)
    * refactor(register): optimized payload generation
    * chore(register): added test for password confirmation

--- src/tests/Register.test.js
+++ src/tests/Register.test.js
@@ -42,6 +42,9 @@ it('allows the user to register successfully', async () => {
     fireEvent.change(screen.getByPlaceholderText('Password'), {
         target: { value: '12345678' },
     })
+    fireEvent.change(screen.getByPlaceholderText('Confirm Password'), {
+        target: { value: '12345678' },
+    })
     expect(screen.getByLabelText('mentor', { name: 'available_to_mentor'}).checked).toEqual(false)
     fireEvent.click(screen.getByLabelText('mentor', { name: "available_to_mentor" }), {
         target: { value: true },
@@ -63,5 +66,44 @@ it('allows the user to register successfully', async () => {
     await waitForElement(() => screen.getByLabelText('response'))
 
     expect(screen.getByLabelText('response')).toHaveTextContent("User was created successfully. A confirmation email has been sent via email. After confirming your email you can login.")
+    
+})
+
+it('blocks the user to register without password confirmation', async () => {
+    render(<Register />)
+
+    fireEvent.change(screen.getByPlaceholderText('Full Name'), {
+        target: { value: 'My Name' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Username'), {
+        target: { value: 'MyUsername' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Email'), {
+        target: { value: 'myemail@xxx.xxx' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Password'), {
+        target: { value: '12345678' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Confirm Password'), {
+        target: { value: '1234' },
+    })
+    expect(screen.getByLabelText('mentor', { name: 'available_to_mentor'}).checked).toEqual(false)
+    fireEvent.click(screen.getByLabelText('mentor', { name: "available_to_mentor" }), {
+        target: { value: true },
+    })
+    expect(screen.getByLabelText('mentee', { name: 'need_mentoring'}).checked).toEqual(false)
+    fireEvent.click(screen.getByLabelText('mentee', { name: "need_mentoring" }), {
+        target: { value: true },
+    })
+    expect(screen.getByLabelText('termsCheck', { name: 'terms_and_conditions_checked'}).checked).toEqual(false)
+    fireEvent.click(screen.getByLabelText('termsCheck', { name: "terms_conditions_checked" }), {
+        target: { value: true },
+    })
+    
+    fireEvent.click(screen.getByRole('button', { name: "Sign Up" }), {
+        target: { value: 'true' },
+    })
+
+    expect(screen.queryByLabelText("response")).not.toBeInTheDocument();
     
 })
