e283e17e80a17377207ad59513fd25e75615a629
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2020-09-07 19:40:09 +0530

    Adding already merged test

--- src/tests/Register.test.js
+++ src/tests/Register.test.js
@@ -70,6 +70,44 @@ it('allows the user to register successfully', async () => {
     
 })
 
+it('blocks the user to register without password confirmation', async () => {
+    render(<Register />)
+
+    fireEvent.change(screen.getByPlaceholderText('Full Name'), {
+        target: { value: 'My Name' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Username'), {
+        target: { value: 'MyUsername' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Email'), {
+        target: { value: 'myemail@xxx.xxx' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Password'), {
+        target: { value: '12345678' },
+    })
+    fireEvent.change(screen.getByPlaceholderText('Confirm Password'), {
+        target: { value: '1234' },
+    })
+    expect(screen.getByLabelText('mentor', { name: 'available_to_mentor'}).checked).toEqual(false)
+    fireEvent.click(screen.getByLabelText('mentor', { name: "available_to_mentor" }), {
+        target: { value: true },
+    })
+    expect(screen.getByLabelText('mentee', { name: 'need_mentoring'}).checked).toEqual(false)
+    fireEvent.click(screen.getByLabelText('mentee', { name: "need_mentoring" }), {
+        target: { value: true },
+    })
+    expect(screen.getByLabelText('termsCheck', { name: 'terms_and_conditions_checked'}).checked).toEqual(false)
+    fireEvent.click(screen.getByLabelText('termsCheck', { name: "terms_conditions_checked" }), {
+        target: { value: true },
+    })
+    
+    fireEvent.click(screen.getByRole('button', { name: "Sign Up" }), {
+        target: { value: 'true' },
+    })
+
+    expect(screen.queryByLabelText("response")).not.toBeInTheDocument();
+    
+})
 
 it('checks empty field warning', async () => {
 
