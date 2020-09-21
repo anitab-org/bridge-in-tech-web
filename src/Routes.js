bbb368551ddef9abc664473bf03d55992f0c3c61
Hitesh Kumar <37622734+HeroicHitesh@users.noreply.github.com>
2020-09-21 13:15:18 +0530

    Feat: Create About.jsx (#100)
    * Feat: Create About.jsx
    
    Create About.jsx for About section
    Add  route in Routes.js
    Move Home.jsx to home folder
    
    Fixes #96
    
    * Add medium article link to Read more...
    
    Add [Medium article link](https://medium.com/anitab-org-open-source/bridgeintech-by-anitab-org-85745853865d?source=friends_link&sk=fcb616ef0ac7e1e85ac6b58b513a56ee) to `a` tag of `Read more about the project here...`

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Home";
+import Home from "./home/Home";
 import Members from "./members/Members";
 import MemberPortfolio from "./members/MemberPortfolio";
 import Member from "./members/Member";
@@ -20,17 +20,19 @@ import ProtectedRoute from "./ProtectedRoute";
 import Navigation from "./Navigation";
 import AuthProvider from "./AuthContext";
 import Programs from "./organizations/Programs";
-
+import About from "./home/About";
 
 export default function Routes() {
-
   return (
     <Router>
       <AuthProvider>
         <Navigation />
         <Switch>
-          <Route exact path="/" >
+          <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
           </Route>
           <ProtectedRoute exact path="/members">
             <Members />
@@ -53,35 +55,26 @@ export default function Routes() {
           <ProtectedRoute path="/organizations/programs/">
             <Program />
           </ProtectedRoute>
-          <ProtectedRoute
-            exact path="/portfolio">
+          <ProtectedRoute exact path="/portfolio">
             <Portfolio />
           </ProtectedRoute>
-          <ProtectedRoute
-            exact path="/personal-details">
+          <ProtectedRoute exact path="/personal-details">
             <PersonalDetails />
           </ProtectedRoute>
-          <ProtectedRoute
-            exact path="/additional-info">
+          <ProtectedRoute exact path="/additional-info">
             <AdditionalInfo />
           </ProtectedRoute>
-          <ProtectedRoute
-            exact path="/personal-background">
+          <ProtectedRoute exact path="/personal-background">
             <PersonalBackground />
           </ProtectedRoute>
-          <ProtectedRoute
-            exact path="/request-history">
-          </ProtectedRoute>
-          <ProtectedRoute
-            exact path="/organization-portfolio">
+          <ProtectedRoute exact path="/request-history"></ProtectedRoute>
+          <ProtectedRoute exact path="/organization-portfolio">
             <EditPrograms />
           </ProtectedRoute>
-          <ProtectedRoute
-            exact path="/organization-profile">
+          <ProtectedRoute exact path="/organization-profile">
             <EditOrganization />
           </ProtectedRoute>
-          <ProtectedRoute
-            path="/organization-programs/">
+          <ProtectedRoute path="/organization-programs/">
             <EditProgram />
           </ProtectedRoute>
           <Route path="/register">
