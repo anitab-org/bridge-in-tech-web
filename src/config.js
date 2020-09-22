14443fc18064f391e1db4a4406832249c7e222dd
AltCtrlDel1999 <shubhamrampurkar3@gmail.com>
2020-09-22 18:39:31 +0530

    change

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const BASE_API = process.env.REACT_APP_BASE_API ?  process.env.REACT_APP_BASE_API : "http://localhost:5000"
+//export const BASE_API = process.env.REACT_APP_BASE_API ?  process.env.REACT_APP_BASE_API : "http://localhost:5000"
+export const BASE_API = process.env.REACT_APP_BASE_API ?  process.env.REACT_APP_BASE_API : "bridgeintech-bit-heroku-psql.herokuapp.com"
