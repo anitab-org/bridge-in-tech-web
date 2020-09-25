5cac36495de558e6182867357fd77d5ffe3dabfc
AltCtrlDel1999 <57938708+AltCtrlDel1999@users.noreply.github.com>
2020-09-25 10:48:35 +0530

    removed heroku line from config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
 export const BASE_API = process.env.REACT_APP_BASE_API ?  process.env.REACT_APP_BASE_API : "http://localhost:5000"
-//export const BASE_API = process.env.REACT_APP_BASE_API ?  process.env.REACT_APP_BASE_API : "bridgeintech-bit-heroku-psql.herokuapp.com"
