ceb5f66fe042c41eabbde0ab328768f456b38e12
Sanjay Prajapati <sanjay270899@gmail.com>
2021-03-14 04:31:46 +0530

    404 Page Overlapping Fix

--- src/errorPage/ErrorPage.js
+++ src/errorPage/ErrorPage.js
@@ -4,16 +4,14 @@ import { Link } from 'react-router-dom';
 
 export default function ErrorPage() {
     return (
-        
-                       <div className="errorPage">
-             <div className="errorPage_content">
+            <div className="errorPage">
+                <div className="errorPage_content">
                     <h1>404</h1>
                     <h4>Oops! Page Not Found</h4>
                     <p>Sorry, the page you were looking for doesn't exist. If you think something is wrong, try again.</p>
                     <button className="button"><Link to="/">Go to Home </Link></button>
                 </div>
             </div>
-       
     )
 }
 
