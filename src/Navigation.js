76ab040f3bc21ba4fe641bb88b71699083c1d52f
stableapple <riddhi01.menon@gmail.com>
2021-04-16 20:46:11 +0530

    Created Navbar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,131 +1,46 @@
-import React, { useContext } from "react";
-import { Navbar, Nav, Card, Accordion, Dropdown } from "react-bootstrap";
+import React  from "react";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom"
-import { AuthContext } from "./AuthContext";
-import DropdownMenu from "react-bootstrap/DropdownMenu";
-import Logo from "./assets/images/AnitaBLogo.png"
 import './Nav.css'
 
 
 export default function Navigation() {
-    const { user, isAuth, login, logout } = useContext(AuthContext);
-
     return (
-        <div className="Nav_container">
-            <div>
-                <Nav.Link as={Link} to='/'>
-                <img
-                        style={{margin:'10px 40px'}}
-                        className="d-inline-block align-top"
-                        src={Logo}
-                        width="150"
-                        height="50"
-                        alt="Logo"
-                    />
-                </Nav.Link>
-            </div>
-            <div className="Nav_box">
-            <h2 style={{color:'#F89728'}}>Bridge In Tech</h2>
-            <Navbar className="mr-auto" expand="lg" style={{borderTop:' 2px solid #54BCEB'}}>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto" id="mainNav">
-                    <Nav.Item>
-                        <Accordion style={{backgroundColor:'white'}}>
-                            <Card style={{backgroundColor:'white'}}>
-                                <Card.Header>
-                                    <Accordion.Toggle as={Link} to="/" eventKey="0">Home</Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="0">
-                                    <Nav>
-                                        <Nav.Link as={Link} to="/about">About</Nav.Link>
-                                        <Nav.Link as={Link} to="/benefit">Benefit</Nav.Link>
-                                        <Nav.Link href="https://anitab.org/contact/" target = "_blank" rel = "noopener noreferrer">Contact</Nav.Link>
-                                    </Nav>
-                                </Accordion.Collapse>
-                            </Card>
-                        </Accordion>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Accordion>
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle as={Link} to="/members" eventKey="0">Members</Accordion.Toggle>
-                                </Card.Header>
-                            </Card>
-                        </Accordion>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Accordion>
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle as={Link} to="/organizations" eventKey="0">Organizations</Accordion.Toggle>
-                                </Card.Header>
-                            </Card>
-                        </Accordion>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Accordion>
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle as={Link} to="/portfolio" eventKey="0">My Space</Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="0">
-                                    <Nav>
-                                        <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
-                                        <Dropdown>
-                                            <Dropdown.Toggle variant="light">Profile</Dropdown.Toggle>
-                                            <DropdownMenu>
-                                                <Nav.Link as={Link} to="/personal-details" eventKey="0">Personal Details</Nav.Link>
-                                                <Nav.Link as={Link} to="/additional-info" eventKey="0">Additional Info</Nav.Link>
-                                                <Nav.Link as={Link} to="/personal-background" eventKey="0">Personal Background</Nav.Link>
-                                            </DropdownMenu>
-                                        </Dropdown>
-                                        <Nav.Link as={Link} to="/request-history">Request History</Nav.Link>
-                                        <Nav.Link as={Link} to="/organization-profile" eventKey="0">My Organization</Nav.Link>
-                                    </Nav>
-                                </Accordion.Collapse>
-                            </Card>
-                        </Accordion>
-                    </Nav.Item>
-                    {!isAuth ?
-                        <>
-                            <Nav.Item>
-                                <Card>
-                                    <Card.Header>
-                                        <Accordion.Toggle as={Link} to="/register" eventKey="0">Register</Accordion.Toggle>
-                                    </Card.Header>
-                                </Card>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Card>
-                                    <Card.Header>
-                                        <Accordion.Toggle as={Link} to="/login" eventKey="0" onClick={login}>Login</Accordion.Toggle>
-                                    </Card.Header>
-                                </Card>
-                            </Nav.Item>
-                        </>
-                        :
-                        <Nav.Item>
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle as={Link} to="/" eventKey="0" onClick={logout}>Logout</Accordion.Toggle>
-                                </Card.Header>
-                            </Card>
-                        </Nav.Item>
-                    }
-                    {isAuth && <Nav.Item>
-                        <Card>
-                            <Card.Header>
-                                <Accordion.Toggle as={Link} to="/" eventKey="0">Welcome back, {user}!</Accordion.Toggle>
-                            </Card.Header>
-                        </Card>
-                    </Nav.Item>}
+      <div className="topnav">
+               <Navbar className="ml-auto" expand="lg" style={{borderTop:' 2px solid #54BCEB'}}>
+               <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+               <Navbar.Collapse id="responsive-navbar-nav">
+               <Nav classname="mr-auto">
+                 <div className="nav-items">
+                      <Link to="\" style={{textDecoration:"none"}}>
+                         <li >About Us</li>
+                      </Link>
+                      <Link to="\" style={{textDecoration:"none"}}>
+                          <li >Services</li>
+                      </Link>
+                      <Link to="\" style={{textDecoration:"none"}}>
+                          <li >Membership</li>
+                      </Link>
+                      <Link to="\" style={{textDecoration:"none"}}>
+                          <li>FaQs</li>
+                      </Link>
+                      <Link to="\" style={{textDecoration:"none"}}>
+                          <li>Contact</li>
+                      </Link>
+                      <Link to="\" style={{textDecorationColor: "blue",textDecoration:"none"}}>
+                          <li >Sign In</li>
+                      </Link>
+                      <Link to="\" style={{textDecoration:"none"}}>
+                          <li className="active" >Sign Up</li>
+                      </Link>
+                 
+                  </div>
                 </Nav>
-            </Navbar.Collapse>
-            </Navbar>
-            </div>
-        </div>
+                </Navbar.Collapse>
+                </Navbar>
+             </div>
+         
 
-    );
+       
+    )
 }
