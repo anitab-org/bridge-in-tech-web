1a303a681b4b9b0ed4a4a377566390d8206c53d6
mtreacy002 <mtreacy002@gmail.com>
2021-04-23 05:14:18 +0000

    Deploying to production from  @ aa0365d5846f404d52d8447872075d6529419a2c 🚀

---
+++ precache-manifest.3b2600841572d0a2d9965277be0afadd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afde72b807914da06ebfda883a2cad25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2740b8c48f27dda1927a",
+    "url": "/static/css/main.d4480424.chunk.css"
+  },
+  {
+    "revision": "25a64b4de85416dde695",
+    "url": "/static/js/2.766c7fd3.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/static/js/2.766c7fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2740b8c48f27dda1927a",
+    "url": "/static/js/main.890fc9b7.chunk.js"
+  },
+  {
+    "revision": "12e68b7bf2a306308573",
+    "url": "/static/js/runtime-main.6db719ef.js"
+  },
+  {
+    "revision": "a7dc40962e9c12c75bed99713ec55afa",
+    "url": "/static/media/AnitaBLogo.a7dc4096.png"
+  },
+  {
+    "revision": "49031bf3a1857208e97df8c549961bfc",
+    "url": "/static/media/benefit1.49031bf3.png"
+  },
+  {
+    "revision": "bf7302d61adac5c7d14528f2bd9ee560",
+    "url": "/static/media/benefit2.bf7302d6.png"
+  },
+  {
+    "revision": "08bf0a96bc7ae4c468ccbbfb4b1410d4",
+    "url": "/static/media/bg.08bf0a96.png"
+  }
+]);
