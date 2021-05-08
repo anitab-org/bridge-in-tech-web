e00a46a2582da857d8c863c977b4f6506ea4bff8
naveen8801 <naveensharma10d@gmail.com>
2021-05-08 14:46:23 +0530

    Read about us section added above get in touch section

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -19,8 +19,8 @@ export default function Home() {
                     </p>
                 </div>
             </section>
-            <GetInTouch />
             <ReadAboutUs />
+            <GetInTouch />
             <Footer/>
         </React.Fragment>
     )
