e76040395e39d397c05db6e3f47203157ae28783
naveen8801 <naveensharma10d@gmail.com>
2021-05-22 17:21:46 +0530

    Components arranged in correct order

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -21,8 +21,8 @@ export default function Home() {
                 </div>
             </section>
             <ReadAboutUs />
-            <GetInTouch />
             <FAQ />
+            <GetInTouch />
             <Footer/>
         </React.Fragment>
     )
