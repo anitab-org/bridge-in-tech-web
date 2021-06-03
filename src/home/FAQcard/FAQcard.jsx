f351109b98625ed0a19c1f60c3330538eeea1f3e
naveen8801 <naveensharma10d@gmail.com>
2021-06-03 09:38:49 +0530

    FAQcard made accordion & Test case error resolved

--- src/home/FAQcard/FAQcard.jsx
+++ src/home/FAQcard/FAQcard.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import './FAQcard.css';
-import logo_left_arrow from './../../assets/images/side_arrow.svg'
+import React, { useState } from "react";
+import "./FAQcard.css";
+import logo_left_arrow from "./../../assets/images/side_arrow.svg";
 export default function FAQcard(props) {
-
-    return (
-        <section className="faq_card">
-          <h2 className="card_text">{props.text}</h2>
-          <img className="card_sidelogo" src={logo_left_arrow} alt="FAQ_side_arrow"  />
-        </section>
-    )
+  const [open, setopen] = useState(false);
+  const onClickHandler = () => {
+    setopen(!open);
+  };
+  return (
+    <>
+      <section className="faq_card">
+        <h2 className="card_text">{props.text}</h2>
+        <img
+          className="card_sidelogo"
+          src={logo_left_arrow}
+          alt="FAQ_side_arrow"
+          onClick={onClickHandler}
+        />
+      </section>
+      {open ? (
+        <div className="accordion_div">
+          <p className="accordion_text">{props.answer}</p>
+        </div>
+      ) : null}
+    </>
+  );
 }
