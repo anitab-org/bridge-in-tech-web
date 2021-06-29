328107496edfd8c3a50c7af922a0ee710b1fc876
arkumari2000 <arkumari2000@gmail.com>
2021-06-29 19:10:27 +0530

    Bug:Sidebar icons on minimized/closed position are not linked to relevant pages

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ import { AuthContext } from "../AuthContext";
 import "./sidebar.css";
 import image from "../assets/images/AnitaBLogo.png";
 
-export default function Sidebar(){
+export default function Sidebar() {
   const { user, isAuth } = useContext(AuthContext);
   const [isNotActive, setNotActive] = useState("true");
   const [isDropdownActive, setDropdownActive] = useState("false");
@@ -15,7 +15,7 @@ export default function Sidebar(){
   return (
     <div className={!isAuth ? "hidden" : ""}>
       <div className="wrapper">
-        <nav id="sidebar" className={isNotActive ? "active" : ""}>
+        <nav id="sidebar" className={isNotActive ? "sidebar-active" : ""}>
           <button
             type="button"
             id="sidebarCollapse"
@@ -25,33 +25,38 @@ export default function Sidebar(){
             <span className={isNotActive ? "" : "hidden"}>{barsIcon}</span>
             <span className={isNotActive ? "hidden" : ""}>{crossIcon}</span>
           </button>
-            <div className="sidebar-header">
-              <img
-                alt="user_logo"
-                src={image}
-                className="rounded-circle usr-image"
-                height={isNotActive ? "20" : "70"}
-                width={isNotActive ? "20" : "70"}
-              ></img>
-              <h3>{user}</h3>
-            </div>
+          <div className="sidebar-header">
+            <img
+              alt="user_logo"
+              src={image}
+              className="rounded-circle usr-image"
+              height={isNotActive ? "20" : "70"}
+              width={isNotActive ? "20" : "70"}
+            ></img>
+            <h3>{user}</h3>
+          </div>
 
-          <ul className ="list-unstyled components">
+          <ul className="list-unstyled components">
             <li className="list-item">
-              <i className="fas fa-briefcase icon-color"></i>
-              <Link to="/members">Members</Link>
+              <Link to="/members" className="sidebar-link">
+                <i className="fas fa-briefcase icon-color"></i>
+                <span className="sidebar-link-name">Members</span>
+              </Link>
             </li>
             <li className="list-item">
-              <i className="fas fa-building icon-color"></i>
-              <Link to="/organizations">Organizations</Link>
+              <Link to="/organizations" className="sidebar-link">
+                <i className="fas fa-building icon-color"></i>
+                <span className="sidebar-link-name">Organizations</span>
+              </Link>
             </li>
             <li className="list-item">
-              <i className="fas fa-user-alt icon-color"></i>
               <OverlayTrigger
                 key="bottom"
                 placement="bottom"
                 overlay={
-                  <Tooltip id={`tooltip-$'bottom'`}>{!isDropdownActive ? "Close My Space" : "Open My Space"}</Tooltip>
+                  <Tooltip id={`tooltip-$'bottom'`}>
+                    {!isDropdownActive ? "Close My Space" : "Open My Space"}
+                  </Tooltip>
                 }
               >
                 <Link
@@ -59,43 +64,47 @@ export default function Sidebar(){
                   href="#homeSubmenu"
                   data-toggle="collapse"
                   aria-expanded="false"
-                  className="dropdown-toggle"
+                  className="dropdown-toggle sidebar-link"
                   onClick={() => setDropdownActive(!isDropdownActive)}
                 >
-                  My Space
+                  <i className="fas fa-user-alt icon-color"></i>
+                  <span className="sidebar-link-name">My Space</span>
                 </Link>
               </OverlayTrigger>
               <ul
                 className={
                   isDropdownActive ? "list-unstyled  collapse" : "list-unstyled"
                 }
-                id="homeSubmenu"
               >
                 <li className="dropdown-item">
-                  <Link to="/portfolio">Portfolio</Link>
+                  <Link to="/portfolio" className="sidebar-link">Portfolio</Link>
                 </li>
                 <li className="dropdown-item">
-                  <Link to="/personal-details">Personal Details</Link>
+                  <Link to="/personal-details" className="sidebar-link">Personal Details</Link>
                 </li>
                 <li className="dropdown-item">
-                  <Link to="/additional-info">Additional Info</Link>
+                  <Link to="/additional-info" className="sidebar-link">Additional Info</Link>
                 </li>
                 <li className="dropdown-item">
-                  <Link to="/personal-background">Personal Background</Link>
+                  <Link to="/personal-background" className="sidebar-link">Personal Background</Link>
                 </li>
               </ul>
             </li>
             <li className="list-item">
-              <i className="fas fa-history icon-color"></i>
-              <Link to="/request-history">Request History</Link>
+              <Link to="/request-history" className="sidebar-link">
+                <i className="fas fa-history icon-color"></i>
+                <span className="sidebar-link-name">Request History</span>
+              </Link>
             </li>
             <li className="list-item">
-              <i className="fas fa-sitemap icon-color"></i>
-              <Link to="/organization-profile">My Organization</Link>
+              <Link to="/organization-profile" className="sidebar-link">
+                <i className="fas fa-sitemap icon-color"></i>
+                <span className="sidebar-link-name">My Organization</span>
+              </Link>
             </li>
           </ul>
         </nav>
       </div>
     </div>
   );
-};
+}
