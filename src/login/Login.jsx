685a4c0ef58bb04fdd1fba7a854dea6b69f0c1e6
reachaadrika <aadrikabhargava12@gmail.com>
2021-06-30 23:34:52 +0530

    feat:Changes in the Sign In page

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -2,15 +2,16 @@ import React, { useState, useContext } from "react";
 import "./Login.css";
 import { Redirect, Link } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
-import {BASE_API} from "../config";
+import { BASE_API } from "../config";
 import { SERVICE_UNAVAILABLE_ERROR } from "../messages";
+import signup from '../assets/images/signup.jpg';
 
 export default function Login() {
     const [errorMessage, setErrorMessage] = useState(null);
-    const {isAuth,login} = useContext(AuthContext);
+    const { isAuth, login } = useContext(AuthContext);
     const [user, setUser] = useState(null);
     const [isPasswordShown, setIsPasswordShown] = useState(false)
-    
+
     const handleSubmit = async e => {
         e.preventDefault();
 
@@ -30,22 +31,22 @@ export default function Login() {
         fetch(`${BASE_API}/login`, requestLogin)
             .then(async response => {
                 let data = await response.json();
-                if (response.ok){
+                if (response.ok) {
                     const access_token = data['access_token']
                     fetch(`${BASE_API}/user/personal_details`, {
                         method: "GET",
                         headers: {
-                             "Authorization": `Bearer ${access_token}`,
-                             "Accept": "application/json",
-                             "Content-Type": "application/json",
+                            "Authorization": `Bearer ${access_token}`,
+                            "Accept": "application/json",
+                            "Content-Type": "application/json",
                         }
                     }).then(async response => {
                         let userData = await response.json();
                         if (response.ok)
-                            return login(data, userData['username']); 
+                            return login(data, userData['username']);
                         setErrorMessage(data.message);
                     }).catch(() => setErrorMessage(SERVICE_UNAVAILABLE_ERROR));
-                }    
+                }
                 setErrorMessage(data.message);
             })
             .catch(() => setErrorMessage(SERVICE_UNAVAILABLE_ERROR));
@@ -58,19 +59,25 @@ export default function Login() {
     return isAuth ?
         <Redirect to="/" />
         : (
-        <div className="container">
-            <div className="row mb-5">
-                <div className="col-lg-12 text-center">
-                    <h1 className="mt-5">Login</h1>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-lg-12">
-                    <form className="login-form mx-auto" onSubmit={handleSubmit}>
+            
+            <div className="container">
+            <img className="side-img" src={signup} alt="image here" ></img>
+              <div className="heading">
+                  <h1>Welcome Back!</h1>
+              </div>
+
+              <div className="content">
+                  <p>Glad to see you again, kindly provide the details below and lets get you back on track again.</p>
+              </div>
+
+            
+
+
+              <form className="login-form mx-auto" onSubmit={handleSubmit}/>
                         <form-group controlId="formUserame">
                             <p className="input-control">
                                 <label id="Username">Username or Email:</label>
-                                <input className="field"
+                                <input className="field1"
                                     type="text"
                                     aria-labelledby="Username"
                                     name="username"
@@ -83,8 +90,8 @@ export default function Login() {
                         <div><br></br></div>
                         <form-group controlId="formPassword">
                             <p className="input-control">
-                                <label id="Password">Password :</label>
-                                <input className="field"
+                                <label id="Password">Password:</label>
+                                <input className="field2"
                                     type={isPasswordShown? "text" : "password"}
                                     aria-labelledby="Password"
                                     name="password"
@@ -93,40 +100,31 @@ export default function Login() {
                                 />
                             </p>
                         </form-group>
-                        <form-group>
-                            <input type="checkbox" className="my-2" name="show_password_checkbox" id="showPassword" onClick={handleTogglePasswordDisplay}/>
-                            <label className="ml-2 my-2" htmlFor="showPassword">Show Password</label>
-                        </form-group>
-                        <div><br></br></div>
+                       
                         <div>
                             {errorMessage && <span className="error" name="errorMessage" aria-label="errorMessage" role="alert">{errorMessage}</span>}
                         </div>
+                        
                         <div className="row">
-                            <label>Not yet registered? Click on sign up.</label>
+                            <label>Dont have an Account? Sign Up.</label>
                         </div>
-                        <div className="row button-group">
-                            <div className="col">
-                                    <Link 
-                                        className="btn btn-primary" 
-                                        id="registerBtn" 
-                                        to="/register" 
-                                        role="button">Sign Up
-                                    </Link>
-                            </div>
-                            <div className="col-0.5"></div>
-                            <div className="col">
-                                <button className="btn btn-success"
-                                    variant="success"
+                        
+                        <button className="btn"
+                               variant="success"
                                     type="submit"
                                     name="submit"
-                                    value="Login"
-                                >Login
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
+                                    value="Sign In"
+                                > 
+                                Sign In
+                            </button>
+
+                         
+                            
+                           
+               
+                   
+                 </div>
+            
+               
+            );
 }
